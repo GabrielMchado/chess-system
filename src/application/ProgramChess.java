9314708cc63b0c7026b8df32643223c0c9fa4970
Gabriel Machado <bieloliveiramachado@gmail.com>
2023-04-07 12:37:37 -0300

    Added UI, Color, ChessMatch, and ChessPiece classes

--- src/application/ProgramChess.java
+++ src/application/ProgramChess.java
@@ -1,9 +1,14 @@
 package application;
 
+import chess.ChessMatch;
+
 public class ProgramChess {
 
 	public static void main(String[] args) {
-
+		
+		
+		ChessMatch chess =  new ChessMatch();
+		UI.printBoard(chess.getPiece());
 		
 		
 	}
--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -1,15 +1,16 @@
 package boardgame;
 
+
 public class Board {
 
 	private int rows;
 	private int columns;
-	private Piece[][] piece;
+	private Piece[][] pieces;
 	
 	public Board(int rows, int columns) {
 		this.rows = rows;
 		this.columns = columns;
-		piece = new Piece[rows][columns];
+		pieces = new Piece[rows][columns];
 	}
 
 	public int getRows() {
@@ -26,6 +27,14 @@ public class Board {
 
 	public void setColumns(int columns) {
 		this.columns = columns;
+	}
+	
+	public Piece piece(int row, int column) {
+		return pieces[row][column];
+	}
+
+	public Piece piece(PositionChessPiece position) {
+		return pieces[position.getRow()][position.getColumn()];
 	}
 	
 }
