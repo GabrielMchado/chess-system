ff5144f3ec1461edec320fab2c2c050321c0584b
Gabriel Machado <bieloliveiramachado@gmail.com>
2023-04-16 14:16:39 -0300

    Moving pieces

--- src/application/ProgramChess.java
+++ src/application/ProgramChess.java
@@ -1,15 +1,32 @@
 package application;
 
+import java.lang.annotation.Target;
+import java.util.Scanner;
+
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class ProgramChess {
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
 		
 		ChessMatch chess =  new ChessMatch();
-		UI.printBoard(chess.getPiece());
 		
+		
+		while(true) {
+			UI.printBoard(chess.getPiece());
+			System.out.println();
+			System.out.print("Source: ");
+			ChessPosition source = UI.readChessPosition(sc);
+			
+			System.out.println();
+			System.out.print("Target: ");
+			ChessPosition target = UI.readChessPosition(sc);
+			ChessPiece capturedPiece = chess.performChessMove(source, target);
+		}
 		
 	}
 
--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -61,4 +61,17 @@ public class Board {
 		return piece(position) != null;
 	}
 	
+	public Piece removePiece(PositionChessPiece position) {
+		if(!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
+		if(piece(position) == null) {
+			return null;
+		}
+		Piece aux = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getColumn()] = null;
+		return aux;
+	}
+	
 }
