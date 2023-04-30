7c5d4d76c16b515c0e58da8e70abf036560033bc
Gabriel Machado <bieloliveiramachado@gmail.com>
2023-04-30 12:03:15 -0300

    Fix - treatment to promotion

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -121,7 +121,7 @@ public class ChessMatch {
 			throw new IllegalStateException("There is no piece to be promoted");
 		}
 		if (!type.equals("B") && !type.equals("N") && !type.equals("R") & !type.equals("Q")) {
-			throw new InvalidParameterException("Invalid type for promoved"); 
+			return promoted;
 		}
 		
 		PositionChessPiece pos = promoted.getChessPosition().toPosition();
